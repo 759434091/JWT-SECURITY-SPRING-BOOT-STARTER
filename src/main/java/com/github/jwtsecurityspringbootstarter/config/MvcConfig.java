cca5ad5b300223b6a7da4a0bd667c407263ca927
xuenenglu <zzz13129180808@gmail.com>
2018-10-30 16:12:16 +0800

    jwt

--- src/main/java/com/github/jwtsecurityspringbootstarter/config/MvcConfig.java
+++ src/main/java/com/github/jwtsecurityspringbootstarter/config/MvcConfig.java
@@ -1,7 +1,10 @@
 package com.github.jwtsecurityspringbootstarter.config;
 
 import com.github.jwtsecurityspringbootstarter.annotation.TokenUserMethodArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,6 +12,11 @@ import java.util.List;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
+    @Bean
+    public AuthenticationManagerBuilder authenticationManagerBuilder(@Autowired AuthenticationManagerBuilder authenticationManagerBuilder) {
+        return authenticationManagerBuilder.eraseCredentials(false);
+    }
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new TokenUserMethodArgumentResolver());
